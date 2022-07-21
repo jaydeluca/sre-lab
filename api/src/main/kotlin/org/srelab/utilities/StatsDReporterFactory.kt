e1c1fd4d6c6e8e71c7d2337f2ebd5680cec862c6
jaydeluca <jaydeluca4@gmail.com>
2022-07-20 21:14:04 -0400

    working on getting statsd running with metricbeat

---
+++ api/src/main/kotlin/org/srelab/utilities/StatsDReporterFactory.kt
@@ -0,0 +1,50 @@
+package org.srelab.utilities
+
+import com.codahale.metrics.MetricFilter
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.ScheduledReporter
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonTypeName
+import com.github.jjagged.metrics.reporting.StatsDReporter
+import com.github.jjagged.metrics.reporting.statsd.StatsD
+import io.dropwizard.metrics.BaseFormattedReporterFactory
+import org.hibernate.validator.constraints.Range
+import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+@JsonTypeName("statsd")
+class StatsDReporterFactory: BaseFormattedReporterFactory() {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(StatsDReporterFactory::class.java)
+    }
+
+    @JsonProperty
+    @NotEmpty
+    private val host = "localhost"
+
+    @JsonProperty
+    @Range(min = 0, max = 49151)
+    private val port = 8125
+
+    @JsonProperty
+    @NotNull
+    private val prefix = ""
+
+
+    override fun build(registry: MetricRegistry): ScheduledReporter {
+        val statsd = StatsD(host, port)
+        val reporter = StatsDReporter
+            .forRegistry(registry)
+            .prefixedWith(prefix)
+            .withTags()
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS).filter(MetricFilter.ALL)
+            .build(statsd);
+        logger.info("Registering StatsDReporter $reporter")
+        return reporter
+    }
+
+}

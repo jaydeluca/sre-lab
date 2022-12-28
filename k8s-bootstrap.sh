680c64dcfc4762aa3fa732ac01e051f0477566c3
jaydeluca <jaydeluca4@gmail.com>
2022-12-28 07:29:40 -0500

    t:q :qgithub actions

--- k8s-bootstrap.sh
+++ k8s-bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 kubectl config use-context docker-desktop
 
-
 # Database
 kubectl apply -f k8s/postgres-config.yaml
 kubectl apply -f k8s/postgres-pvc-pv.yaml
 kubectl apply -f k8s/postgres-deployment.yaml
 kubectl apply -f k8s/postgres-service.yaml
-
 
 # SigNoz
 DEFAULT_STORAGE_CLASS=$(kubectl get storageclass -o=jsonpath='{.items[?(@.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")].metadata.name}')
@@ -19,9 +17,9 @@ kubectl create ns platform
 helm --namespace platform install signoz signoz/signoz
 
 POD_NAME=$(kubectl get pods --namespace platform -l "app.kubernetes.io/name=signoz,app.kubernetes.io/instance=signoz,app.kubernetes.io/component=frontend" -o jsonpath="{.items[0].metadata.name}")
-kubectl --namespace platform port-forward "$POD_NAME" 3301:3301
 
 # Apps
+kubectl apply -f k8s/users-api.yml
+kubectl apply -f k8s/orders-api.yml
 
-#kubectl apply -f k8s/users-api.yml
-#kubectl apply -f k8s/orders-api.yml
+kubectl --namespace platform port-forward "$POD_NAME" 3301:3301
